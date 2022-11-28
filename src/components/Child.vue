498ab1ec08f06b6dd5ff0a27c72aed30f3a9d04a
MrGultekin <gultekin.de@gmail.com>
2022-11-28 13:08:27 +0100

    Provide/Inject done

--- src/components/Child.vue
+++ src/components/Child.vue
@@ -1,12 +1,19 @@
 <script setup>
 import GrandChild from './GrandChild.vue'
+import {inject} from "vue";
 
+const numbers = inject('numbers')
+
+const addNumber = (num) => {
+  numbers.value.push(num)
+}
 </script>
 
 <template>
   <div>
 <!--     Let's render the numbers that we passed from the Parent component-->
     <h1>Child Component {{numbers}}</h1>
+    <button @click="addNumber(5)">Add 5</button>
     <div class="line"></div>
     <GrandChild />
 
--- src/components/GreatGrandChild.vue
+++ src/components/GreatGrandChild.vue
@@ -1,9 +1,16 @@
 <script setup>
+import {inject} from "vue";
 
+const numbers = inject('numbers')
+
+const addNumber0 = (num) => {
+  numbers.value.push(num)
+}
 </script>
 <template>
   <div>
     <h1>GreatGrandChild Component {{numbers}}</h1>
+    <button @click="addNumber0(0)">Add 0</button>
     <div class="line"></div>
   </div>
 </template>
