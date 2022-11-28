4621bdc0718e39104fdb5dcace4d6503bffe9f97
MrGultekin <gultekin.de@gmail.com>
2022-11-28 09:32:43 +0100

    Let's have a piece of state that we can change

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Parent from './components/Parent.vue'
 import {ref} from "vue";
 
-
+// Let's have a piece of state that we can change
 const numbers = ref([1,2,3,4,5,6,7,8,9,10])
 </script>
 
